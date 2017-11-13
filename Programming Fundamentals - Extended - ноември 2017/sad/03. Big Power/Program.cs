b8a2a88590dd0aa3f2a805595f68da8b5d8575e1
Your Name <ivvo98@abv.bg>
2017-11-13 23:38:36 +0200

    Funda

---
+++ Programming Fundamentals - Extended - ноември 2017/sad/03. Big Power/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3_Big_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = BigInteger.Parse(Console.ReadLine());
+
+            var currentNumber = number;
+            for (int i = 1; i < currentNumber; i++)
+            {
+                number *= currentNumber;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
